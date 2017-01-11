02ce27dc004c970a55d10e9f0884ef525daac6bd
Jonathan Norstrom <jon.norstrom@gmail.com>
2017-01-11 10:27:01 -0500

    toggle details

--- src/Session.js
+++ src/Session.js
@@ -9,18 +9,41 @@ class Speaker extends Component {
     )
   }
 }
+
 class Session extends Component {
+  constructor(props) {
+    super(props)
+    this.state = ({
+      showDetails: false,
+      showHide: "Show"
+    })
+  }
+
+  toggleState(e) {
+    e.preventDefault()
+    this.setState({
+      showDetails: !this.state.showDetails,
+      showHide: this.state.showHide === "Show" ? "Hide" : "Show"
+    })
+  }
   render() {
     const {title, abstract} = this.props
     return (
       <div>
         <p>{title}</p>
-        <p>{abstract}</p>
+        <a href="" onClick={this.toggleState.bind(this)}>{this.state.showHide} Details</a>
+        { this.state.showDetails && <p>{abstract}</p> }
 
         <Speaker {...this.props.speakers[0]}/>
       </div>
     )
   }
+}
+
+Session.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  abstract: React.PropTypes.string.isRequired,
+  speakers: React.PropTypes.array.isRequired
 }
 
 export default Session
